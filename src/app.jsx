cf3d326b6d2323cea4a8ca20fd0ab3abd098c12d
kiranbandi <bvenkatkiran@gmail.com>
2019-09-04 19:52:12 -0600

    added service worker

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ReactSelect from 'react-select';
 import _ from "lodash";
+import * as serviceWorker from './serviceWorker';
 
 const CARDS_LIST = ["EM-TRANSITION TO DISCIPLINE", "EM-CORE", "EM-FOUNDATION",
   "EM-TRANSITION TO PRACTICE", "ANESTHESIA",
@@ -95,3 +96,7 @@ class App extends Component {
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
